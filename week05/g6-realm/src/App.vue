b0b0bec453eea01d61f0d981e914bb1d3d322744
Zachy-lee <55906558+Zachy-lee@users.noreply.github.com>
2020-05-11 07:58:30 +0800

    Add files via upload

---
+++ week05/g6-realm/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <realm></realm>
+  </div>
+</template>
+
+<script>
+import realm from './components/realm'
+
+export default {
+  name: 'App',
+  components: {
+    realm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
