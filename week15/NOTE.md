9d2dd4cd3315fab5230d3c358d459a95b441350b
Zachy <duyuan30@126.com>
2020-07-24 00:35:49 +0800

    animation

--- week15/NOTE.md
+++ week15/NOTE.md
@@ -10,15 +10,29 @@
 ###  animation动画状态
 animation-play-state:paused;
 
-### transition动画
-getComputedStyle(el).transform
+### transition动画 暂停位置处理
+getComputedStyle(el)
 
 
+### 走GPU处理 translate 3D 
 
+## timing function 设计 及bezier以time为入参，以progression为出参
 
+## 浏览器里c++代码分析 cubic-bezier 
+  webpack源码
+  牛顿积分
 ###  参考链接
+
+
 https://trac.webkit.org/browser/trunk/Source/WebCore/platform/animation
 
 http://en.wikipedia.org/wiki/Newton’s_method
 
 https://cubic-bezier.com/#.25,.1,.25,1
+
+
+### 小tip
+void 0 与 undefined的区别
+
+- undefined可以被重写
+- void 0计算返回 undefined 不能被重写且表达式最短
--- week15/animation/animation.js
+++ week15/animation/animation.js
@@ -1,44 +1,75 @@
 export class Timeline {
     constructor() {
         this.animations = [];
-        this.requestID = null
+        this.requestID = null;
+        this.state = 'inited'
     }
     tick() {
         let t = Date.now() - this.startTime;
-        console.log(t);
-        this.animations = this.animations.filter(animation => !animation.finished)
-        for (const animation of this.animations) {
-            let { object, property, template, start, end, delay, timingFunction } = animation;
-            let progression = timingFunction((t - delay) / animation.duration) // 0-1 之间的数
-            if (t > animation.duration + animation.delay) {
+        let animations = this.animations.filter(animation => !animation.finished)
+        for (let animation of animations) {
+            let { object, property, template, start, end, duration, delay, timingFunction, addTime } = animation;
+
+            let progression = timingFunction((t - delay - addTime) / duration) // 0-1 之间的数
+
+            if (t > duration + delay + addTime) {
                 progression = 1;
                 animation.finished = true;
             }
-            let value = start + progression * (end - start)
+
+            let value = animation.valueFromProgression(progression)
+                // if (property === 'backgroundColor') {
+                //     console.log(value);
+                //     console.log(property);
+                //     console.log(object);
+                // }
             object[property] = template(value)
+            console.log(property, object[property]);
         }
-        if (this.animations.length)
+        if (animations.length)
             this.requestID = requestAnimationFrame(() => this.tick())
     }
     pause() {
+        if (this.state !== 'playing') return;
+        this.state = 'paused';
         this.pauseTime = Date.now();
         if (this.requestID)
             cancelAnimationFrame(this.requestID)
     }
     resume() {
+        if (this.state !== 'paused') return;
+        this.state = 'playing';
         this.startTime += Date.now() - this.pauseTime;
         this.tick();
     }
     start() {
-        this.startTime = Date.now()
+        if (this.state !== 'inited') return;
+        this.state = 'playing';
+        this.startTime = Date.now();
         this.tick();
     }
-    add(animation) {
+    add(animation, addTime) {
         this.animations.push(animation);
+        animation.finished = false;
+        if (this.state === 'playing') {
+            animation.addTime = addTime !== void 0 ? addTime : Date.now() - this.startTime;
+        } else {
+            animation.addTime = addTime !== void 0 ? addTime : 0;
+        }
+    }
+    restart() {
+        if (this.state === 'playing')
+            this.pause();
+        this.animations = [];
+        this.requestID = null;
+        this.state = 'playing';
+        this.startTime = Date.now();
+        this.pauseTime = null;
+        this.tick();
     }
 }
 export class Animation {
-    constructor(object, property, template, start, end, duration, delay, timingFunction) {
+    constructor(object, property, start, end, duration, delay, timingFunction, template) {
         this.object = object;
         this.template = template;
         this.property = property;
@@ -47,6 +78,29 @@ export class Animation {
         this.delay = delay;
         this.duration = duration;
         this.timingFunction = timingFunction;
+    }
+    valueFromProgression(progression) {
+        return this.start + progression * (this.end - this.start)
+    }
+}
+export class ColorAnimation {
+    constructor(object, property, start, end, duration, delay, timingFunction, template) {
+        this.object = object;
+        this.template = template || (v => `rgba(${v.r},${v.g},${v.b},${v.a})`);
+        this.property = property;
+        this.start = start;
+        this.end = end;
+        this.delay = delay;
+        this.duration = duration;
+        this.timingFunction = timingFunction;
+    }
+    valueFromProgression(progression) {
+        return {
+            r: this.start.r + progression * (this.end.r - this.start.r),
+            g: this.start.g + progression * (this.end.g - this.start.g),
+            b: this.start.b + progression * (this.end.b - this.start.b),
+            a: this.start.a + progression * (this.end.a - this.start.a),
+        }
     }
 }
 
