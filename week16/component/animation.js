1e8ab6dd707a817c041eca2f3dae583a63d69bac
Zachy-lee <55906558+Zachy-lee@users.noreply.github.com>
2020-07-30 16:50:23 +0800

    Update animation.js

--- week16/component/animation.js
+++ week16/component/animation.js
@@ -1,46 +1,47 @@
 export class Timeline {
     constructor() {
-        this.animations = [];
+        this.animations = new Set();
+        this.finishedAnimations = new Set();
+        this.addTimes = new Map()
         this.requestID = null;
         this.state = 'inited'
-    }
-    tick() {
-        let t = Date.now() - this.startTime;
-        let animations = this.animations.filter(animation => !animation.finished)
-        for (let animation of animations) {
-            let { object, property, template, start, end, duration, delay, timingFunction, addTime } = animation;
+        this.tick = () => {
+            let t = Date.now() - this.startTime;
+            // let animations = this.animations.filter(animation => !animation.finished)
+            for (let animation of this.animations) {
+                let { object, property, template, start, end, duration, delay, timingFunction } = animation;
+                let addTime = this.addTimes.get(animation);
+                let progression = timingFunction((t - delay - addTime) / duration) // 0-1 之间的数
 
-            let progression = timingFunction((t - delay - addTime) / duration) // 0-1 之间的数
+                if (t > duration + delay + addTime) {
+                    progression = 1;
+                    // animation.finished = true;
+                    this.animations.delete(animation);
+                    this.finishedAnimations.add(animation);
+                }
+                if (t < delay + addTime) {
+                    continue;
+                }
 
-            if (t > duration + delay + addTime) {
-                progression = 1;
-                animation.finished = true;
+                let value = animation.valueFromProgression(progression)
+                object[property] = template(value)
+                // console.log(property, object[property]);
             }
-
-            let value = animation.valueFromProgression(progression)
-                // if (property === 'backgroundColor') {
-                //     console.log(value);
-                //     console.log(property);
-                //     console.log(object);
-                // }
-            object[property] = template(value)
-            console.log(property, object[property]);
+            if (this.animations.size)
+                this.requestID = requestAnimationFrame(this.tick)
+            else
+                this.requestID = null
         }
-        if (animations.length)
-            this.requestID = requestAnimationFrame(() => this.tick())
     }
+
     pause() {
         if (this.state !== 'playing') return;
         this.state = 'paused';
         this.pauseTime = Date.now();
-        if (this.requestID)
+        if (this.requestID !== null) {
             cancelAnimationFrame(this.requestID)
-    }
-    resume() {
-        if (this.state !== 'paused') return;
-        this.state = 'playing';
-        this.startTime += Date.now() - this.pauseTime;
-        this.tick();
+            this.requestID = null
+        }
     }
     start() {
         if (this.state !== 'inited') return;
@@ -49,18 +50,34 @@ export class Timeline {
         this.tick();
     }
     add(animation, addTime) {
-        this.animations.push(animation);
-        animation.finished = false;
+        this.animations.add(animation);
+        if (this.state === 'playing' && this.requestID === null)
+            this.tick()
         if (this.state === 'playing') {
-            animation.addTime = addTime !== void 0 ? addTime : Date.now() - this.startTime;
+            this.addTimes.set(animation, addTime !== void 0 ? addTime : Date.now() - this.startTime)
         } else {
-            animation.addTime = addTime !== void 0 ? addTime : 0;
+            this.addTimes.set(animation, addTime !== void 0 ? addTime : 0);
         }
+    }
+    reset() {
+        if (this.state === 'playing')
+            this.pause();
+        this.animations = new Set;
+        this.finishedAnimations = new Set;
+        this.addTimes = new Map;
+        this.requestID = null;
+        this.state = 'playing';
+        this.startTime = Date.now();
+        this.pauseTime = null;
+        this.tick();
     }
     restart() {
         if (this.state === 'playing')
             this.pause();
-        this.animations = [];
+        for (const animation of this.finishedAnimations) {
+            this.animations.add(animation)
+        }
+        this.finishedAnimations = new Set();
         this.requestID = null;
         this.state = 'playing';
         this.startTime = Date.now();
