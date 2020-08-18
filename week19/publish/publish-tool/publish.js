a92c9354d03b32d2ee3b26e8b057cf00297c4413
Zachy-lee <55906558+Zachy-lee@users.noreply.github.com>
2020-08-18 14:45:50 +0800

    Update publish.js

--- week19/publish/publish-tool/publish.js
+++ week19/publish/publish-tool/publish.js
@@ -3,34 +3,34 @@ const querystring = require('querystring');
 const postData = querystring.stringify({
     'content': 'Hello World!202020'
 });
+const fs =  require('fs');
 
-const options = {
-    host: 'localhost',
-    port: 8081,
-    method: 'POST',
-    path: '/?filename=z.html',
-    headers: {
-        'Content-Type': 'application/octet-stream',
-        'Content-Length': Buffer.byteLength(postData)
-    }
-};
+let filename = './publish/publish-tool/cat.jpg';
+fs.stat(filename,(error,stats)=>{
+    console.log(stats);
+    const options = {
+        host: 'localhost',
+        port: 8081,
+        method: 'POST',
+        path: '/?filename=cat.jpg',
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Content-Length': stats.size
+        }
+    };
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`); 
+    });
+    
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
 
-const req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    // res.setEncoding('utf8');
-    // res.on('data', (chunk) => {
-    //     console.log(`BODY: ${chunk}`);
-    // });
-    // res.on('end', () => {
-    //     console.log('No more data in response.');
-    // });
-});
-
-req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-});
-
-// Write data to request body
-req.write(postData);
-req.end();
+    // Write data to request body
+    let rs = fs.createReadStream(filename);
+    rs.pipe(req);
+    rs.on('end',()=>{
+        req.end();
+    })
+})
